714f5965a3279e19a4c453ce3ee5d6abbb06f5e5
bigodedaprafirma-cyber <bigodedaprafirma@gmail.com>
2025-09-03 21:24:43 -0300

    Fix Express catch-all route pattern

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = 3000;
 app.use(express.static('.'));
 
 // Handle all routes by serving index.html
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
